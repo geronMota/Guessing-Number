027a0ceec79996d949595e6bbbba2b870368a981
Jefferson Mota <geron_22@hotmail.com>
2022-11-03 20:18:07 -0300

    fix some issue
    In colaboration with my friend Danilo San

--- script.js
+++ script.js
@@ -1,36 +1,82 @@
- 
 let check = document.querySelector('#check')
 let btnRefresh = document.querySelector('.btnRefresh')
-let p = 0;
+var p = 0;
 
 
 let sorteio = Math.floor(Math.random()* 20) + 1
 console.log(sorteio)
-document.querySelector('.randomNumber').innerHTML = sorteio
-document.querySelector('.randomNumber').style.color = "white";
+document.querySelector('#guessNumber').value;
+document.querySelector('.randomNumber').innerHTML = '?';
+document.querySelector('.randomNumber').style.color = 'red'
 
+
+var win = 0;
 function play(){
-    
     let randomNumber = document.querySelector('.randomNumber').innerText
     let guessNumber = parseInt(document.querySelector('#guessNumber').value)
     let answer = document.querySelector('#answer')
     let guNumber = document.querySelector('#guessedNumbers')
     let nofguess = document.querySelector('#numberOfGuesses')
 
+
     
     if(guessNumber < 1 || guessNumber > 20){
         alert("please,type a number between 1 and 20")
     }
     else if(guessNumber === sorteio){
-        
+        win = sorteio;
+        document.querySelector('.randomNumber').innerHTML = sorteio;
+
          answer.innerHTML = "Congrats!! you won!🤩🙌✨"
        document.querySelector('.randomNumber').style.color = 'red'
         
-    }else if(guessNumber < sorteio){
-        answer.innerHTML = "🥶 The number is low";
-    }else if(guessNumber > sorteio){
-        answer.innerHTML = "🥵 The number is high";
-    }
+    }else if((guessNumber >= 1 || guessNumber <= 5) && sorteio > 16){
+        answer.innerHTML = "🥶It's too cold,try a higher number";
+    }else if((guessNumber >= 1 || guessNumber <= 5) && (sorteio > 12 || sorteio < 17)){
+        answer.innerHTML = "🥶It's too cold yet,try a higher number";
+    }else if((guessNumber >= 1 || guessNumber <= 5) && (sorteio > 8 || sorteio < 13)){
+        answer.innerHTML = "🙂It's getting warm,try a higher number";
+    }else if((guessNumber >= 1 || guessNumber <= 5) && (sorteio > 4 || sorteio < 9)){
+        answer.innerHTML = "😎It's warm,try a higher number";
+    }else if((guessNumber >= 1 || guessNumber <= 5) && (sorteio > 0 || sorteio < 5)){
+        answer.innerHTML = "🥵It's too hot,you are about to get it";
+    
+}else if((guessNumber >= 6 || guessNumber <= 10) && sorteio > 16){
+    answer.innerHTML = "🥶It's too cold,try a higher number";
+}else if((guessNumber >= 6 || guessNumber <= 10) && (sorteio > 12 || sorteio < 17)){
+    answer.innerHTML = "🥶It's too cold yet,try a higher number";
+}else if((guessNumber >= 6 || guessNumber <= 10) && (sorteio > 8 || sorteio < 13)){
+    answer.innerHTML = "🙂It's getting warm,try a higher number";
+}else if((guessNumber >= 6 || guessNumber <= 10) && (sorteio > 4 || sorteio < 9)){
+    answer.innerHTML = "😎It's warm,try a higher number";
+}else if((guessNumber >= 6 || guessNumber <= 10) && (sorteio > 0 || sorteio < 5)){
+    answer.innerHTML = "🥵It's too hot,you are about to get it";
+}
+
+else if((guessNumber >= 11 || guessNumber <= 15) && sorteio > 16){
+    answer.innerHTML = "🥶It's too cold,try a higher number";
+}else if((guessNumber >= 11 || guessNumber <= 15) && (sorteio > 12 || sorteio < 17)){
+    answer.innerHTML = "🥶It's too cold yet,try a higher number";
+}else if((guessNumber >= 11 || guessNumber <= 15) && (sorteio > 8 || sorteio < 13)){
+    answer.innerHTML = "🙂It's getting warm,try a higher number";
+}else if((guessNumber >= 11 || guessNumber <= 15) && (sorteio > 4 || sorteio < 9)){
+    answer.innerHTML = "😎It's warm,try a higher number";
+}else if((guessNumber >= 11 || guessNumber <= 15) && (sorteio > 0 || sorteio < 5)){
+    answer.innerHTML = "🥵It's too hot,you are about to get it";
+}
+
+else if((guessNumber >= 16 || guessNumber <= 20) && sorteio > 16){
+    answer.innerHTML = "🥶It's too cold,try a higher number";
+}else if((guessNumber >= 16 || guessNumber <= 20) && (sorteio > 12 || sorteio < 17)){
+    answer.innerHTML = "🥶It's too cold yet,try a higher number";
+}else if((guessNumber >= 16 || guessNumber <= 20) && (sorteio > 8 || sorteio < 13)){
+    answer.innerHTML = "🙂It's getting warm,try a higher number";
+}else if((guessNumber >= 16 || guessNumber <= 20) && (sorteio > 4 || sorteio < 9)){
+    answer.innerHTML = "😎It's warm,try a higher number";
+}else if((guessNumber >= 16 || guessNumber <= 20) && (sorteio > 0 || sorteio < 5)){
+    answer.innerHTML = "🥵It's too hot,you are about to get it";
+}
+
     
 }
 
@@ -44,13 +90,16 @@ function timer(){
     let score = document.querySelector('#pontos')
     
       p++
-         score.innerHTML = p; 
-         if(p === 30){
+        score.innerHTML = p; 
+        if(p <= 30 && win === sorteio){
+            p = 0;
+            clearInterval(timer)
+        } else if (p === 30){
             document.querySelector('#answer').innerHTML = "Game Over"
             p = 0;
             clearInterval(timer)
         }
-    }   
+}   
     
     
 
